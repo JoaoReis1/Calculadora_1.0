097ff43f62f6bd469c18dfda35ee57217546ec32
JoaoReis1 <joaoreis1245@gmail.com>
2024-03-31 14:26:33 -0300

    Update Calculadora1.0.py
    Corrige o problema para sempre considerar o ultimo sinal logico

--- Calculadora1.0.py
+++ Calculadora1.0.py
@@ -1,5 +1,14 @@
 import customtkinter
-import time
+import tkinter
+import re
+import PIL
+
+
+
+'1 Resolver o problema quando é digitado dois operadores logico, sempre trocar pelo ultimo digitado'
+
+
+
 janela = customtkinter.CTk()
 
 digitados = list()
@@ -15,37 +24,40 @@ def main():
     frame_result()
     config_button()
 
-def init_config():  
+def init_config():
     janela.title("Calculadora 1.0")
     janela.geometry("300x400")
     janela.maxsize(width=300,height=400 )
     janela.minsize(width=300,height=400 )
 
-
-
 def frame_result():
-    
-    frama1 = customtkinter.CTkFrame( master=janela,width=285, height=90, corner_radius=30).place(x=5,y=10)
-    
+
+    txtbox_result = customtkinter.CTkTextbox(janela,corner_radius=20, text_color = "white" ,width=275,height=85)
+    txtbox_result.place(x=10,y=10)
+
 def config_button():
-    
+
     btn1 = customtkinter.CTkButton(master=janela, text="1", width=90, height=50,corner_radius=30,command=valor1).place(x=10,y=275)
     btn2 = customtkinter.CTkButton(master=janela, text="2", width=90, height=50,corner_radius=30,command=valor2).place(x=105,y=275)
-    btn3 = customtkinter.CTkButton(master=janela, text="6", width=90, height=50,corner_radius=30,command=valor3).place(x=200,y=275)
-    
+    btn3 = customtkinter.CTkButton(master=janela, text="3", width=90, height=50,corner_radius=30,command=valor3).place(x=200,y=275)
+
     btn4 = customtkinter.CTkButton(master=janela, text="4", width=90, height=50,corner_radius=30,command=valor4).place(x=10,y=220)
     btn5 = customtkinter.CTkButton(master=janela, text="5", width=90, height=50,corner_radius=30,command=valor5).place(x=105,y=220)
     btn6 = customtkinter.CTkButton(master=janela, text="6", width=90, height=50,corner_radius=30,command=valor6).place(x=200,y=220)
-    
+
     btn7 = customtkinter.CTkButton(master=janela, text="7", width=90, height=50,corner_radius=30,command=valor7).place(x=10, y= 165)
     btn8 = customtkinter.CTkButton(master=janela, text="8", width=90, height=50,corner_radius=30,command=valor8).place(x=105, y=165)
     btn9 = customtkinter.CTkButton(master=janela, text="9", width=90, height=50,corner_radius=30,command=valor9).place(x=200, y=165)
-    
-    
-    btn_result = customtkinter.CTkButton(master=janela, text="Resultado :)", width=120, height=50,corner_radius=30,command=resultado).place(x=90, y= 340)
-    
-    btn_soma = customtkinter.CTkButton(master=janela, text="+",width=90,height=50,corner_radius=30,command=soma).place(x=200,y=110)
-    
+    btn0 = customtkinter.CTkButton(master=janela, text="0", width=90, height=50,corner_radius=30,command=valor9).place(x=105, y=335)
+
+    btn_del =customtkinter.CTkButton(master=janela,text="Del", width=90,height=50,corner_radius=30,fg_color="teal",command=delete).place(x=10,y=335)
+
+    btn_result = customtkinter.CTkButton(master=janela, text="=", width=90, height=50,corner_radius=30,fg_color="teal",command=resultado).place(x=200, y= 335)
+
+    btn_soma = customtkinter.CTkButton(master=janela, text="+",width=90,height=50,corner_radius=30,fg_color="teal",command=soma).place(x=200,y=110)
+    btn_mult = customtkinter.CTkButton(master=janela, text='x',width=90,height=50,corner_radius=30,fg_color="teal",command=mult).place(x=105,y=110)
+
+
 def valor1():
     digitados.append(1)
     controle_erro()
@@ -72,84 +84,87 @@ def valor8():
     controle_erro()
 def valor9():
     controle_erro()
-    
-def soma ():
-    
-    txtbox_result = customtkinter.CTkTextbox(janela,corner_radius=0, text_color = "white" ,width=275,height=85)
-    txtbox_result.place(x=10,y=10)
-    
-    
-    tranforma_string = [str(i)for i in digitados]
-    n1 = ''.join(tranforma_string)
-    db.append(int(n1))
-    digitados.clear()
 
-    n1 = ''
-    
+
+def soma():
+    digitados.append('+')
+    controle_erro()
+def mult():
+    digitados.append('*')
+    controle_erro()
+def delete():
+    digitados.pop()
+    controle_erro()
+    print("Numero deletado")
+
 def resultado():
-    if not db:
-        #Caso nenhum operador seja escolhido mostrará os numeros digitados, IMPORTANT:Informação só tornará no prompt, na tela inda mostrará os numeros digitados conforme interação.
-        
-        tranforma_string = [str(i)for i in digitados]
-        n1 = ''.join(tranforma_string)
-        print(n1)
-        
-    else:
-    
-        ' primeiro laço descobre quantas lista tem dentro das lista'
-        
-        txtbox_result = customtkinter.CTkTextbox(janela,corner_radius=0, text_color = "white" ,width=275,height=85)
-        txtbox_result.place(x=10,y=10)
-        
-        'Preciso tranformar em string o ultimo valor antes de mostrar o resultado'
-        tranforma_string3 = [str(i)for i in digitados]
-        n1 = ''.join(tranforma_string3)
-        db.append(int(n1))
-        
-        
-        ' Aqui temos que fazer um loop com o len'
-        resultado = 0
-        descricao =''
-        
-        for ind, val in enumerate(db):
-            resultado = resultado + val
-    
-        for i in db:
-            descricao= descricao + f"{i}+"
-        
-        txtbox_result.insert("1.0",f"{descricao}={resultado}")  
-        
-        'Não mexe tá funcionando'
-        n1=''
-        digitados.clear()
-        db.clear()
-        db.append(resultado)
-        resultado=0
+    db.append(digitados)
+
+    transforma =''
+
+    for i in str(db):
+        transforma = transforma + i
+
+
+    termos = re.findall(r"\d+|\*|\+", transforma)
+
+    while '*' in termos:
+        i = termos.index('*')
+        resu = int(termos[i-1]) * int(termos[i+1])
+        termos = termos[:i-1] + [str(resu)] + termos[i+2:]
+
+
+    while '+' in termos:
+        i = termos.index('+')
+        resu = int(termos[i-1]) + int(termos[i+1])
+        termos = termos[:i-1] + [str(resu)] + termos[i+2:]
+
+    resultado_final = "".join(termos)  # Convertendo termos em uma string
+
+    txtbox_result = customtkinter.CTkTextbox(janela,corner_radius=20, text_color="white", width=275, height=85)
+    txtbox_result.place(x=10, y=10)
+    txtbox_result.configure(font=("Helvetica", 20))
+
+    txtbox_result.insert("1.0",f"{resultado_final}")
+    print(f"Resultado: {resultado_final}")
+
+
+    db.clear()
+    digitados.clear()
+    db.append(resultado_final)
 
 def controle_erro():
-    
+
     if not db:
-        #print("Aguardando operador") 
-        
-        txtbox_result = customtkinter.CTkTextbox(janela,corner_radius=0, text_color = "white" ,width=275,height=85)
+        #print("Aguardando operador")
+
+        ''' Resolve o problema no ultimo sinal digitado'''
+        for i,v in enumerate(digitados):
+            if str(v) in '+*' and str(digitados[i-1]) in '+*':
+                digitados.pop(i-1)
+
+        txtbox_result = customtkinter.CTkTextbox(janela,corner_radius=20, text_color = "white" ,width=275,height=85)
         txtbox_result.place(x=10,y=10)
-        
+        txtbox_result.configure(font=("Helvetica", 20))
+
+
         tranforma_string1 = [str(i)for i in digitados]
         n1 = ''.join(tranforma_string1)
-        txtbox_result.insert("1.0",f"{n1}") 
+        txtbox_result.insert("10.0",f"{n1}")
         print(n1)
-        
+
     else:
         print(f"Aguardando dados:{len(db)+1}")
-        
-        txtbox_result = customtkinter.CTkTextbox(janela,corner_radius=0, text_color = "white" ,width=275,height=85)
+
+        txtbox_result = customtkinter.CTkTextbox(janela,corner_radius=20, text_color = "white" ,width=275,height=85)
         txtbox_result.place(x=10,y=10)
-        
+        txtbox_result.configure(font=("Helvetica", 20))
+
         tranforma_string1 = [str(i)for i in digitados]
         n1 = ''.join(tranforma_string1)
-        txtbox_result.insert("1.0",f"{n1}") 
+        txtbox_result.insert("10.0",f"{n1}")
         print(n1)
-        
+
 main()
 
 janela.mainloop()
